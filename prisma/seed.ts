e20e430c72ac820103d27bdcacae087800bda977
Vijak Khajornritdacha <vijak13019@hotmail.com>
2024-11-02 20:29:31 +0700

    refactor: prettify codes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,29 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const mockAccountsData: Prisma.accountsCreateInput[] = [
-  {
-    email: "random1",
-    password: "password",
-    username: "random1",
-    created_on: new Date(),
-  },
+	{
+		email: 'random1',
+		password: 'password',
+		username: 'random1',
+		created_on: new Date()
+	}
 ];
 
 const prisma = new PrismaClient();
 async function main() {
-  console.log("Start seeding...");
-  const accounts = await prisma.accounts.createMany({
-    data: mockAccountsData,
-  });
-  console.log("Created accounts: ", accounts);
+	console.log('Start seeding...');
+	const accounts = await prisma.accounts.createMany({
+		data: mockAccountsData
+	});
+	console.log('Created accounts: ', accounts);
 }
 
 main()
-  .then(async (res) => {
-    await prisma.$disconnect();
-  })
-  .catch(async (err) => {
-    console.log(err);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+	.then(async (res) => {
+		await prisma.$disconnect();
+	})
+	.catch(async (err) => {
+		console.log(err);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
