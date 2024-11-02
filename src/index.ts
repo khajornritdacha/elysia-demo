681df3db709ac2a44a4a3fabf3a52be97113bdc9
Vijak Khajornritdacha <vijak13019@hotmail.com>
2024-11-02 20:27:14 +0700

    fix: rename controller

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { swagger } from "@elysiajs/swagger";
-import { Elysia } from "elysia";
-import { accounts } from "./module/account/account.controller";
+import { swagger } from '@elysiajs/swagger';
+import { Elysia } from 'elysia';
+import { accountsController } from './module/account/account.controller';
 
 const app = new Elysia()
-  .use(swagger())
-  .use(accounts)
-  .get("/", () => "Hello Elysia")
-  .listen(3000);
+	.use(swagger())
+	.use(accountsController)
+	.get('/', () => 'Hello Elysia')
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
