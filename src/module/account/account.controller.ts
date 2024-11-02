6ba98f65eb3bec51cd21f397034fd1eab8e219cd
Vijak Khajornritdacha <vijak13019@hotmail.com>
2024-11-02 20:25:29 +0700

    refactor: prettify codes

--- src/module/account/account.controller.ts
+++ src/module/account/account.controller.ts
@@ -1,23 +1,23 @@
-import Elysia from "elysia";
-import { AccountModel } from "./account.model";
-import { AccountService } from "./account.service";
+import Elysia from 'elysia';
+import { AccountModel } from './account.model';
+import { AccountService } from './account.service';
 
-export const accounts = new Elysia({ prefix: "accounts" })
-  .use(AccountModel)
-  .post(
-    "/",
-    async ({ body }) => {
-      return await AccountService.addAccount({
-        ...body,
-        created_on: new Date(),
-      });
-    },
-    {
-      error(err) {
-        console.log(err);
-        return err;
-      },
-      body: "account.create",
-      response: "account.create.response",
-    }
-  );
+export const accountsController = new Elysia({ prefix: 'accounts' })
+	.use(AccountModel)
+	.post(
+		'/',
+		async ({ body }) => {
+			return await AccountService.addAccount({
+				...body,
+				created_on: new Date()
+			});
+		},
+		{
+			error(err) {
+				console.log(err);
+				return err;
+			},
+			body: 'account.create',
+			response: 'account.create.response'
+		}
+	);
--- src/module/account/account.service.ts
+++ src/module/account/account.service.ts
@@ -1,27 +1,27 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export type AccountCreateBody = Prisma.Args<
-  typeof prisma.accounts,
-  "create"
->["data"];
+	typeof prisma.accounts,
+	'create'
+>['data'];
 
 export abstract class AccountService {
-  static async addAccount(accountBody: AccountCreateBody) {
-    try {
-      const account = await prisma.accounts.create({
-        data: accountBody,
-        select: {
-          user_id: true,
-          username: true,
-        },
-      });
+	static async addAccount(accountBody: AccountCreateBody) {
+		try {
+			const account = await prisma.accounts.create({
+				data: accountBody,
+				select: {
+					user_id: true,
+					username: true
+				}
+			});
 
-      return account;
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  }
+			return account;
+		} catch (err) {
+			console.log(err);
+			throw err;
+		}
+	}
 }
